fbc0086b748341345fe6433aec2c09f7c7020151
honglin-c <chl9797@live.com>
2024-04-17 22:17:25 -0400

    more cleanup

--- src/SCL1LossProjection.hpp
+++ src/SCL1LossProjection.hpp
@@ -1,25 +1,3 @@
-// Copyright (c) 2021 George E. Brown and Rahul Narain
-//
-// WRAPD uses the MIT License (https://opensource.org/licenses/MIT)
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is furnished
-// to do so, subject to the following conditions:
-//
-// The above copyright notice and this permission notice shall be included in all
-// copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
-// INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
-// PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
-// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
-// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
-// SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-//
-// By George E. Brown (https://www-users.cse.umn.edu/~brow2327/)
-
 #ifndef SRC_SCL1LOSSSTEPBEHAVIOR_HPP_
 #define SRC_SCL1LOSSSTEPBEHAVIOR_HPP_
 
@@ -40,14 +18,6 @@ class SCL1LossProjection{
 
         sc_l1_w = system->sc_l1_w;
         sc_l1_threshold = system->sc_l1_threshold;
-
-        // VA.resize(m_X0.rows());
-        // std::fill(VA.begin(), VA.end(), 0.0);
-        // for (int i = 0; i < system->m_tet_elements->m_tet.size(); i++) {
-        //     for (int j = 0; j < 4; j++) {
-        //         VA[system->m_tet_elements->m_tet[i][j]] += system->m_tet_elements->m_volume[i] / 4.0;
-        //     }
-        // }
     } 
     
     void project(const math::MatX3 &m_X) {
